e1157ad438b84883212839004ba4245cb739e8e3
vicattoIryna <vicatto@ukr.net>
2025-02-12 22:46:15 +0100

    first commit

---
+++ src/main/java/PhoneBook/data/UserData.java
@@ -0,0 +1,7 @@
+package PhoneBook.data;
+
+public class UserData {
+    public static final String VALID_EMAIL = "vicatto@ukr.net";
+    public static final String RANDOM_EMAIL = "vicatto"+ System.currentTimeMillis() +"@ukr.net";
+    public static final String VALID_PASSWORD = "Password@1";
+}
